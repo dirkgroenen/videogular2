895f34c012ee26885f60ad439973721a4fa33c1a
Jeremy Collins <jdodsoncollins@gmail.com>
2020-02-07 11:07:33 -0800

    Initial ivy-compatible build

--- buffering.ts
+++ buffering.ts
@@ -1,1 +1,2 @@
 export * from './src/buffering/buffering';
+export * from './src/buffering/vg-buffering';
---
+++ public-api.ts
@@ -0,0 +1,64 @@
+
+ // Modules
+export * from './src/buffering/buffering';
+export * from './src/buffering/vg-buffering';
+export * from './src/controls/controls';
+export * from './src/controls/vg-controls';
+export * from './src/controls/vg-fullscreen/vg-fullscreen';
+export * from './src/controls/vg-play-pause/vg-play-pause';
+export * from './src/controls/vg-playback-button/vg-playback-button';
+export * from './src/controls/vg-quality-selector/vg-quality-selector';
+export * from './src/controls/vg-scrub-bar/vg-scrub-bar';
+export * from './src/controls/vg-scrub-bar/vg-scrub-bar-buffering-time/vg-scrub-bar-buffering-time';
+export * from './src/controls/vg-scrub-bar/vg-scrub-bar-cue-points/vg-scrub-bar-cue-points';
+export * from './src/controls/vg-scrub-bar/vg-scrub-bar-current-time/vg-scrub-bar-current-time';
+export * from './src/controls/vg-time-display/vg-time-display';
+export * from './src/controls/vg-track-selector/vg-track-selector';
+export * from './src/controls/vg-volume/vg-volume';
+export * from './src/controls/controls';
+export * from './src/controls/vg-controls';
+export * from './src/core/core';
+export * from './src/core/vg-player/vg-player';
+export * from './src/core/vg-media/vg-media';
+export * from './src/core/vg-cue-points/vg-cue-points';
+export * from './src/controls/vg-mute/vg-mute';
+export * from './src/ima-ads/ima-ads';
+export * from './src/ima-ads/vg-ima-ads';
+export * from './src/overlay-play/overlay-play';
+export * from './src/overlay-play/vg-overlay-play';
+export * from './src/streaming/streaming';
+export * from './src/streaming/vg-hls/vg-hls';
+export * from './src/streaming/vg-dash/vg-dash';
+
+
+// Required for Ivy Build of Library
+
+// export * from './lib/buffering/vg-buffering';
+// export * from './lib/controls/vg-controls';
+// export * from './lib/controls/vg-fullscreen/vg-fullscreen';
+// export * from './lib/controls/vg-mute/vg-mute';
+// export * from './lib/controls/vg-play-pause/vg-play-pause';
+// export * from './lib/controls/vg-playback-button/vg-playback-button';
+// export * from './lib/controls/vg-volume/vg-volume';
+// export * from './lib/controls/vg-quality-selector/vg-quality-selector';
+// export * from './lib/controls/vg-scrub-bar/vg-scrub-bar';
+// export * from './lib/controls/vg-scrub-bar/vg-scrub-bar-buffering-time/vg-scrub-bar-buffering-time';
+// export * from './lib/controls/vg-scrub-bar/vg-scrub-bar-cue-points/vg-scrub-bar-cue-points';
+// export * from './lib/controls/vg-scrub-bar/vg-scrub-bar-current-time/vg-scrub-bar-current-time';
+// export * from './lib/controls/vg-time-display/vg-time-display';
+// export * from './lib/controls/vg-track-selector/vg-track-selector';
+// export * from './lib/core/vg-player/vg-player';
+// export * from './lib/core/vg-media/vg-media';
+// export * from './lib/core/vg-cue-points/vg-cue-points';
+// export * from './lib/ima-ads/vg-ima-ads';
+// export * from './lib/overlay-play/vg-overlay-play';
+
+
+
+
+
+
+
+
+
+
